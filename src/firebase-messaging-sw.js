3ff4ab5b679e1b85defca859f062780c63fada73
Giovanni Noceti <giovanni.noceti@gmail.com>
2021-08-18 18:52:27 -0400

    1

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -3,7 +3,7 @@ importScripts("https://www.gstatic.com/firebasejs/3.5.0/firebase-messaging.js")
 
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register("/firebase-messaging-sw.js")
+    .register("./firebase-messaging-sw.js")
     .then(function (registration) {
       console.log("Registration successful, scope is:", registration.scope)
     })
