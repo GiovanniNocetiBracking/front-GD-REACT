b0568c2168e6aeab5bf5fd566aaa1d430081c81b
Giovanni Noceti <giovanni.noceti@symbiose.cl>
2021-12-30 11:21:43 -0300

    tabla de reports renderizando correctamente

--- src/components/Report/ReportTable.js
+++ src/components/Report/ReportTable.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import {
 	Row,
 	Col,
+	Form,
 	Table,
 	Card,
 	Input,
@@ -17,10 +18,13 @@ import { useAuth } from "../../contexts/AuthContext";
 export default function ReportTable() {
 	const { currentUser } = useAuth();
 	const [dropdown, setDropdown] = useState(false);
-	const [selected, setSelected] = useState("");
+	const [selected, setSelected] = useState("ambos");
 	const [startDate, setStartDate] = useState();
 	const [endDate, setEndDate] = useState();
-	const [reportsData, setReportsData] = useState();
+	const [reportsByMail, setReportsByMail] = useState([]);
+	const [reportsBySms, setReportsBySms] = useState([]);
+	const [reports, setReports] = useState([]);
+	const [flag, setFlag] = useState(false);
 	const handleOpen = () => {
 		setDropdown(!dropdown);
 	};
@@ -30,107 +34,149 @@ export default function ReportTable() {
 	const handleEndDate = (event) => {
 		setEndDate(event.target.value);
 	};
-	const handleSearch = () => {
+	const handleFlag = () => {
+		for (let i = 0; i < 1; i++) {
+			setTimeout(() => {
+				setFlag(!flag);
+			}, 1000);
+		}
+	};
+	const handleSearch = async () => {
 		firestore
 			.collection("reports")
 			.doc(currentUser.uid)
 			.onSnapshot((doc) => {
-				setReportsData([doc.data().alertMail, doc.data().alertSMS]);
+				setReportsByMail(doc.data().alertMail);
+				setReportsBySms(doc.data().alertSMS);
 			});
-		console.log(reportsData);
+
+		setReports([...reportsByMail, ...reportsBySms]);
+
+		reports.forEach((el) => {
+			let dateTranformed = new Date(
+				el.date.seconds * 1000 + el.date.nanoseconds / 1000000
+			);
+			let finalDate =
+				dateTranformed.getDate() +
+				"-" +
+				(dateTranformed.getMonth() + 1) +
+				"-" +
+				dateTranformed.getFullYear();
+		});
 	};
+
 	useEffect(() => {
 		handleSearch();
-	}, [currentUser]);
+	}, [flag]);
+
+	useEffect(() => {
+		handleFlag();
+	}, []);
 
 	return (
 		<>
 			<div className="content">
 				<div className="mt-5">
-					<Row className=" d-flex justify-content-center">
-						<Col sm="6" lg="2">
-							<Dropdown isOpen={dropdown} toggle={handleOpen}>
-								<DropdownToggle caret> Selecciona una opcion...</DropdownToggle>
-								<DropdownMenu right>
-									<DropdownItem
-										onClick={() => {
-											setSelected("sms");
-										}}
-									>
-										SMS
-									</DropdownItem>
-									<DropdownItem
-										onClick={() => {
-											setSelected("mail");
-										}}
-									>
-										Mail
-									</DropdownItem>
-									<DropdownItem
-										onClick={() => {
-											setSelected("ambos");
-										}}
-									>
-										Ambos
-									</DropdownItem>
-								</DropdownMenu>
-							</Dropdown>
-						</Col>
-						<Col sm="6" lg="2">
-							<Input type="date" onChange={handleStartDate}></Input>
-						</Col>
-						<Col sm="6" lg="2">
-							<Input type="date" onChange={handleEndDate}></Input>
-						</Col>
-						<Col sm="6" lg="2">
-							<Button
-								onClick={() =>
-									alert(
-										"tipo: " +
-											selected +
-											" fecha inicio: " +
-											startDate +
-											" fecha fin: " +
-											endDate
-									)
-								}
+					<Form>
+						<Row className="d-flex justify-content-center ">
+							<Col sm="6" md="6" lg="4">
+								<Input type="date" required onChange={handleStartDate}></Input>
+							</Col>
+							<Col sm="6" md="6" lg="4">
+								<Input type="date" required onChange={handleEndDate}></Input>
+							</Col>
+						</Row>
+						<Row className="d-flex justify-content-center">
+							<Col
+								sm="12"
+								md="6"
+								lg="3"
+								className="mt-5 d-flex justify-content-end"
 							>
-								Buscar
-							</Button>
-						</Col>
-					</Row>
+								<Dropdown isOpen={dropdown} toggle={handleOpen}>
+									<DropdownToggle caret>
+										{" "}
+										Selecciona una opcion...
+									</DropdownToggle>
+									<DropdownMenu>
+										<DropdownItem
+											onClick={() => {
+												setSelected("sms");
+											}}
+										>
+											SMS
+										</DropdownItem>
+										<DropdownItem
+											onClick={() => {
+												setSelected("mail");
+											}}
+										>
+											Mail
+										</DropdownItem>
+										<DropdownItem
+											onClick={() => {
+												setSelected("ambos");
+											}}
+										>
+											Ambos
+										</DropdownItem>
+									</DropdownMenu>
+								</Dropdown>
+							</Col>
+
+							<Col sm="12" md="6" lg="3" className="mt-5">
+								<Button
+									onClick={() =>
+										alert(
+											"tipo: " +
+												selected +
+												" fecha inicio: " +
+												startDate +
+												" fecha fin: " +
+												endDate
+										)
+									}
+								>
+									Buscar
+								</Button>
+							</Col>
+						</Row>
+					</Form>
+
 					<br />
-					<Row className=" d-flex justify-content-center">
+					<Row className=" d-flex justify-content-center mt-5">
 						<Col sm="12" lg="8">
-							<Card className="">
-								<Table bordered className="p-5">
+							<Card className="mb-5">
+								<Table bordered className="p-5 ">
 									<thead>
 										<tr>
 											<th>#</th>
-											<th>First Name</th>
-											<th>Last Name</th>
-											<th>Username</th>
+											<th>GLP</th>
+											<th>CO</th>
+											<th>FECHA</th>
+											<th>TIPO</th>
 										</tr>
 									</thead>
 									<tbody>
-										<tr>
-											<th scope="row">1</th>
-											<td>Mark</td>
-											<td>Otto</td>
-											<td>@mdo</td>
-										</tr>
-										<tr>
-											<th scope="row">2</th>
-											<td>Jacob</td>
-											<td>Thornton</td>
-											<td>@fat</td>
-										</tr>
-										<tr>
-											<th scope="row">3</th>
-											<td>Larry</td>
-											<td>the Bird</td>
-											<td>@twitter</td>
-										</tr>
+										{reports.length !== 0 ? (
+											reports.map((el, index) => (
+												<tr key={index}>
+													<td>{index + 1}</td>
+													<td>{el.glp}</td>
+													<td>{el.co}</td>
+													<td>{index + 1}</td>
+													<td>{el.withMail || el.withSms}</td>
+												</tr>
+											))
+										) : (
+											<tr>
+												<td>no hay datos</td>
+												<td>no hay datos</td>
+												<td>no hay datos</td>
+												<td>no hay datos</td>
+												<td>no hay datos</td>
+											</tr>
+										)}
 									</tbody>
 								</Table>
 							</Card>
